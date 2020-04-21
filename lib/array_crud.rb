f0613e3dbc18bf5cc42168ae2b6284a0ad974dc5
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-21 04:30:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def remove_element_from_end_of_array(array)
 arrays_array = array.pop
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(array, element)
 wow_array = array.shift
 wow_array
 end
